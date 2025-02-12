6c7d33b7b32f8b96d95fdd5d78431e91840e7876
ziMu-66ccff <3553189793@qq.com>
2025-02-12 11:56:03 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Page() {
@@ -10,6 +11,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -26,6 +28,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src={'/hero-desktop.png'}
+            alt="xxx"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+          ></Image>
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lavishly_Yours } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lavishlyYours = Lavishly_Yours({ weight: '400', subsets: ['latin'] });
